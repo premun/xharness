d346dba81d07e70649fc15673c63166eca4893e6
Premek Vysoky <premek.vysoky@microsoft.com>
2021-03-03 19:48:55 +0100

    Refactor LogWriter

--- src/Microsoft.DotNet.XHarness.TestRunners.Common/LogWriter.cs
+++ src/Microsoft.DotNet.XHarness.TestRunners.Common/LogWriter.cs
@@ -5,12 +5,13 @@
 using System;
 using System.IO;
 
+#nullable enable
 namespace Microsoft.DotNet.XHarness.TestRunners.Common
 {
     public class LogWriter
     {
         private readonly TextWriter _writer;
-        private readonly IDevice _device;
+        private readonly IDevice? _device;
 
         public MinimumLogLevel MinimumLogLevel { get; set; } = MinimumLogLevel.Info;
 
@@ -20,84 +21,36 @@ namespace Microsoft.DotNet.XHarness.TestRunners.Common
 
         public LogWriter(TextWriter w) : this(null, w) { }
 
-        public LogWriter(IDevice device, TextWriter writer)
+        public LogWriter(IDevice? device, TextWriter writer)
         {
             _writer = writer ?? Console.Out;
             _device = device;
+
             if (_device != null) // we just write the header if we do have the device info
             {
-                InitLogging();
+                // print some useful info
+                _writer.WriteLine("[Runner executing:\t{0}]", "Run everything");
+                _writer.WriteLine("[{0}:\t{1} v{2}]", _device.Model, _device.SystemName, _device.SystemVersion);
+                _writer.WriteLine("[Device Name:\t{0}]", _device.Name);
+                _writer.WriteLine("[Device UDID:\t{0}]", _device.UniqueIdentifier);
+                _writer.WriteLine("[Device Locale:\t{0}]", _device.Locale);
+                _writer.WriteLine("[Device Date/Time:\t{0}]", DateTime.Now); // to match earlier C.WL output
+                _writer.WriteLine("[Bundle:\t{0}]", _device.BundleIdentifier);
             }
         }
 
         [System.Runtime.InteropServices.DllImport("/usr/lib/libobjc.dylib")]
         private static extern IntPtr objc_msgSend(IntPtr receiver, IntPtr selector);
 
-        public void InitLogging()
-        {
-            // print some useful info
-            _writer.WriteLine("[Runner executing:\t{0}]", "Run everything");
-            _writer.WriteLine("[{0}:\t{1} v{2}]", _device.Model, _device.SystemName, _device.SystemVersion);
-            _writer.WriteLine("[Device Name:\t{0}]", _device.Name);
-            _writer.WriteLine("[Device UDID:\t{0}]", _device.UniqueIdentifier);
-            _writer.WriteLine("[Device Locale:\t{0}]", _device.Locale);
-            _writer.WriteLine("[Device Date/Time:\t{0}]", DateTime.Now); // to match earlier C.WL output
-            _writer.WriteLine("[Bundle:\t{0}]", _device.BundleIdentifier);
-        }
-        public void OnError(string message)
-        {
-            if (MinimumLogLevel < MinimumLogLevel.Error)
-            {
-                return;
-            }
+        public void OnError(string message) => Write(message, MinimumLogLevel.Error);
 
-            _writer.WriteLine(message);
-            _writer.Flush();
-        }
+        public void OnWarning(string message) => Write(message, MinimumLogLevel.Warning);
 
-        public void OnWarning(string message)
-        {
-            if (MinimumLogLevel < MinimumLogLevel.Warning)
-            {
-                return;
-            }
+        public void OnInfo(string message) => Write(message, MinimumLogLevel.Info);
 
-            _writer.WriteLine(message);
-            _writer.Flush();
-        }
+        public void OnDebug(string message) => Write(message, MinimumLogLevel.Debug);
 
-        public void OnDebug(string message)
-        {
-            if (MinimumLogLevel < MinimumLogLevel.Debug)
-            {
-                return;
-            }
-
-            _writer.WriteLine(message);
-            _writer.Flush();
-        }
-
-        public void OnDiagnostic(string message)
-        {
-            if (MinimumLogLevel < MinimumLogLevel.Verbose)
-            {
-                return;
-            }
-
-            _writer.WriteLine(message);
-            _writer.Flush();
-        }
-
-        public void OnInfo(string message)
-        {
-            if (MinimumLogLevel < MinimumLogLevel.Info)
-            {
-                return;
-            }
-
-            _writer.WriteLine(message);
-            _writer.Flush();
-        }
+        public void OnDiagnostic(string message) => Write(message, MinimumLogLevel.Verbose);
 
         public void Info(string message)
         {
@@ -105,5 +58,15 @@ namespace Microsoft.DotNet.XHarness.TestRunners.Common
             _writer.Flush();
         }
 
+        private void Write(string message, MinimumLogLevel level)
+        {
+            if (MinimumLogLevel < level)
+            {
+                return;
+            }
+
+            _writer.WriteLine(message);
+            _writer.Flush();
+        }
     }
 }
